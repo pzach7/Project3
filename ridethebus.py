a7bc11dae1ec56edf772fc7d9359914da79ba433
Zachery Puderbach <pzach7@vt.edu>
2015-12-08 00:04:06 -0500

    Added varialbes for lines

--- ridethebus.py
+++ ridethebus.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python
 #Ride the bus explanation
+
+twoXline = "   X X  "
+threeXline =  "  X X X " 
+fourXline = " X X X X"
+
 print "Ride The Bus"
 print "Ride The Bus is a card game, with cards laid out as follows"
-print "   X X   (6) "
-print "  X X X  (5) "
-print " X X X X (4) "
-print " X X X X (3) "
-print "  X X X  (2) "
-print "   X X   (1) "
+print twoXline, "(6)"
+print threeXline, "(5)"
+print fourXline, "(4)"
+print fourXline, "(3)"
+print threeXline, "(2)"
+print twoXline, "(1)"
 print "Each X represents a face down card."
 print "Users pick a card, starting at row (1)."
-print "If the card picked is not a face card (Jack, Queen, King, or Ace), the user advances to the next row."
-print "If a face card is picked, the game is over"
+print "If the card picked is not a face card,  the user advances to the next row."
+print "If a face card (Jack, Queen, King, or Ace), is picked, the game is over"
 print "The goal is to get to end of the six rows without drawing a face card."
